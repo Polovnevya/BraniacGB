bc02183c3e4affb39f3dbbb445046cc448c85e08
yuriy <yuriy@MATE.myguest.virtualbox.org>
2023-01-22 21:29:37 +0300

    add url names and news pagination

--- mainapp/views.py
+++ mainapp/views.py
@@ -23,15 +23,22 @@ class NewsPageView(TemplateView):
         return context
 
 
+class NewsWithPaginatorView(NewsPageView):
+    def get_context_data(self, page, **kwargs):
+        context = super().get_context_data(page=page, **kwargs)
+        context["page_num"] = page
+        return context
+
+
 class CoursesPageView(TemplateView):
     template_name = "mainapp/courses_list.html"
 
 
-class ContactsPageVeiw(TemplateView):
+class ContactsPageView(TemplateView):
     template_name = "mainapp/contacts.html"
 
 
-class DocSiteView(TemplateView):
+class DocSitePageView(TemplateView):
     template_name = "mainapp/doc_site.html"
 
 
