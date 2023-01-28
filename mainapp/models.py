1549602ce2cc40861387ac62b50caf8f72b59df2
yuriy <yuriy@MATE.myguest.virtualbox.org>
2023-01-28 21:09:01 +0300

    lesson_4 Display model's data

--- mainapp/models.py
+++ mainapp/models.py
@@ -29,7 +29,6 @@ class News(BaseModel):
 
 
 class Courses(BaseModel):
-
     name = models.CharField(max_length=256, verbose_name="Name")
     description = models.TextField(verbose_name="Description", blank=True, null=True)
     description_as_markdown = models.BooleanField(verbose_name="As markdown", default=False)
