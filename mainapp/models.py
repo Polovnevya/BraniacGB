10e6eaf848ee908a1fd41714d8d50e4db02ca89c
yuriy <yuriy@MATE.myguest.virtualbox.org>
2023-02-05 07:32:06 +0300

    lesson_6 Customizing admin panel

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class BaseModelManager(models.Manager):
@@ -21,25 +22,28 @@ class BaseModel(models.Model):
         abstract = True
 
 
+
+
 class News(BaseModel):
     title = models.CharField(max_length=256, verbose_name="Title")
     preambule = models.CharField(max_length=1024, verbose_name="Preambule")
     body = models.TextField(blank=True, null=True, verbose_name="Body")
     body_as_markdown = models.BooleanField(default=False, verbose_name="As markdown")
 
+    class Meta:
+        verbose_name = _("News")
+        verbose_name_plural = _("News")
+        ordering = ("-created",)
+
+
+class CoursesManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(deleted=False)
+
 
 class Courses(BaseModel):
-    name = models.CharField(max_length=256, verbose_name="Name")
-    description = models.TextField(verbose_name="Description", blank=True, null=True)
-    description_as_markdown = models.BooleanField(verbose_name="As markdown", default=False)
-    cost = models.DecimalField(max_digits=8, decimal_places=2, verbose_name="Cost", default=0)
-    cover = models.CharField(max_length=25, default="no_image.svg", verbose_name="Cover")
-    
-    def __str__(self) -> str:
-        return f"{self.pk} {self.name}"
+    objects = CoursesManager()
 
-
-class TempCourses(BaseModel):
     name = models.CharField(max_length=256, verbose_name="Name")
     description = models.TextField(verbose_name="Description", blank=True, null=True)
     description_as_markdown = models.BooleanField(verbose_name="As markdown", default=False)
@@ -53,24 +57,26 @@ class Lesson(BaseModel):
     title = models.CharField(max_length=256, verbose_name="Name")
     description = models.TextField(verbose_name="Description", blank=True, null=True)
     description_as_markdown = models.BooleanField(verbose_name="As markdown", default=False)
+    
 
     def __str__(self) -> str:
         return f"{self.course.name} | {self.num} | {self.title}"
 
     class Meta:
         ordering = ("course", "num")
+        verbose_name = _("Lesson")
+        verbose_name_plural = _("Lessons")
 
-class CourseTeachers(models.Model):
+
+class CourseTeachers(BaseModel):
     course = models.ManyToManyField(Courses)
     name_first = models.CharField(max_length=128, verbose_name="Name")
     name_second = models.CharField(max_length=128, verbose_name="Surname")
     day_birth = models.DateField(verbose_name="Birth date")
-    deleted = models.BooleanField(default=False)
 
     def __str__(self) -> str:
         return "{0:0>3} {1} {2}".format(self.pk, self.name_second, self.name_first)
 
-    def delete(self, *args):
-        self.deleted = True
-        self.save()
-
+    class Meta:
+        verbose_name = _("Teacher")
+        verbose_name_plural = _("Teachers")
