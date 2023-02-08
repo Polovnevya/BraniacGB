d052a5e4f153480b46d550aec6271584216c4406
yuriy <yuriy@MATE.myguest.virtualbox.org>
2023-02-08 18:24:07 +0300

    Start lesson 7

--- config/settings.py
+++ config/settings.py
@@ -25,12 +25,8 @@ SECRET_KEY = "django-insecure-2z%zf4khasf+#f8&0!)ut8*48n!v#a7)ig1bmvtx6x(3nsjlg$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
-if DEBUG:
-    INTERNAL_IPS = [
-        "192.168.1.4",
-        "127.0.0.1",
-    ]
+ALLOWED_HOSTS = []
+
 
 # Application definition
 
@@ -46,7 +42,6 @@ INSTALLED_APPS = [
     "mainapp",
     "authapp",
     "crispy_forms",
-    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
@@ -57,7 +52,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = "config.urls"
@@ -161,65 +155,3 @@ AUTHENTICATION_BACKENDS = (
 )
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
-
-LOG_FILE = BASE_DIR / "var" / "log" / "main_log.log"
-
-LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "formatters": {
-        "console": {
-            "format": "[%(asctime)s] %(levelname)s %(name)s (%(lineno)d) %(message)s"
-        },
-    },
-    "handlers": {
-        "file": {
-            "level": "DEBUG",
-            "class": "logging.FileHandler",
-            "filename": LOG_FILE,
-            "formatter": "console",
-        },
-        "console": {"class": "logging.StreamHandler", "formatter": "console"},
-    },
-    "loggers": {
-        "django": {"level": "INFO", "handlers": ["console"]},
-        "mainapp": {
-            "level": "DEBUG",
-            "handlers": ["file"],
-        },
-    },
-}
-
-# In the end of file
-CACHES = {
-    "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379",
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        },
-    }
-}
-
-CELERY_BROKER_URL = "redis://localhost:6379"
-CELERY_RESULT_BACKEND = "redis://localhost:6379"
-
-
-# Read about sending email:
-#   https://docs.djangoproject.com/en/3.2/topics/email/
-
-# Full list of email settings:
-#   https://docs.djangoproject.com/en/3.2/ref/settings/#email
-# EMAIL_HOST = "localhost"
-# EMAIL_PORT = "25"
-
-# For debugging: python -m smtpd -n -c DebuggingServer localhost:25
-# EMAIL_HOST_USER = "django@geekshop.local"
-# EMAIL_HOST_PASSWORD = "geekshop"
-# EMAIL_USE_SSL = False
-# If server support TLS:
-# EMAIL_USE_TLS = True
-
-# Email as files for debug
-EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
-EMAIL_FILE_PATH = "var/email-messages/"
